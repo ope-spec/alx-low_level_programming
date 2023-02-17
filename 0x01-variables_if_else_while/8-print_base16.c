01576fd9f0f06be5cba0135c1f71936335901fb8
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-02-17 04:12:44 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print all possible combinations fo single-digit numbers.
- * Numbers must be separated by commas and a space.
- * You can only use `putchar` to print to console
- * You can only use `putchar` up to four times.
- * You are not allowed to use any variable of type `char`.
+ * Description: Print all numbers of base64 in lowercase.
+ * You can only use `putchar`, and only 3 times.
  * Return: 0
  */
 int main(void)
 {
-	int i = 0;
+	char c;
+	int i;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
-		if (i < 9)
-		{
-			putchar(44);
-			putchar(32);
-		}
-		i++;
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
 	}
 	putchar('\n');
 
 	return (0);
 }
+
