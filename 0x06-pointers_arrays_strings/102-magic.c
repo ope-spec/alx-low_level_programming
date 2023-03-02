c6dd4ae6232bac3e64b1b21e2d3d5658aafd6e30
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-03-02 03:28:37 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+*main - Entry point
+*
+*Description: 'magic in c'
+*@p[5] = 98 :print out 98
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+int n;
+int a[5];
+int *p;
 
-  a[2] = 1024;
-  p = &n;
+a[2] = 1024;
+p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -16,9 +23,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 5) = 98;
+p[5] = 98;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
-
