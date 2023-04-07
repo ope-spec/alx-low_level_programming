1fb96777832645c61f61f2f276e6e509ec003f96
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-04-07 21:05:16 +0100

    testing out

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,37 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
  *
- * @b: a pointer to a string of 0 and 1 chars
- *
- * Return: the converted number, or 0 if there is one or more chars in the
- * string b that is not 0 or 1, or if b is NULL.
- **/
+ * Return: unsigned int.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    int length = 0;
+	unsigned int ui;
+	int len, base_two;
 
-    if (b == NULL)
-    {
-        return 0;
-    }
-    while (b[length] != '\0')
-    {
-        length++;
-    }
-    for (int i = 0; i < length; i++)
-    {
-        if (b[i] != '0' && b[i] != '1'){
-            return 0;
-        }
+	if (!b)
+		return (0);
 
-        result <<= 1;
-        if (b[i] == '1')
-        {
-            result += 1;
-        }
-    }
+	ui = 0;
 
-    return result;
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+	}
+
+	return (ui);
 }
