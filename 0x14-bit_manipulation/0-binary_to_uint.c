959757960de673580a5585d13f052f236b86f3d5
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-04-07 21:14:15 +0100

    still checking out something

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,25 +9,25 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	unsigned int result;
+	int length, base_two;
 
 	if (!b)
 		return (0);
 
 	ui = 0;
 
-	for (len = 0; b[len] != '\0'; len++)
+	for (length = 0; b[length] != '\0'; length++)
 		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (length--, base_two = 1; length >= 0; lenth--, base_two *= 2)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[lenth] != '0' && b[lenth] != '1')
 		{
 			return (0);
 		}
 
-		if (b[len] & 1)
+		if (b[lenth] & 1)
 		{
 			ui += base_two;
 		}
