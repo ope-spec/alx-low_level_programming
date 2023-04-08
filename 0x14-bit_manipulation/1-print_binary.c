fce342db428813e779ced47728d70bcd2d515e72
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-04-08 09:32:17 +0100

    2nd commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,16 +9,33 @@
  */
 void print_binary(unsigned long int n)
 {
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
-    if (n == 1)
-    {
-        _putchar('1');
-        return;
-    }
-    print_binary(n >> 1);
-    _putchar((n & 1) + '0');
+   unsigned long int mask = 1;
+	int printed = 0;
+
+	mask <<= (sizeof(unsigned long int) * 8) - 1;
+
+	while (mask > 0)
+	{
+		if ((n & mask) == 0 && printed == 0)
+		{
+			mask >>= 1;
+			continue;
+		}
+
+		if ((n & mask) != 0)
+		{
+			_putchar('1');
+			printed = 1;
+		}
+		else
+		{
+			_putchar('0');
+			printed = 1;
+		}
+
+		mask >>= 1;
+	}
+
+	if (!printed)
+		_putchar('0');
 }
