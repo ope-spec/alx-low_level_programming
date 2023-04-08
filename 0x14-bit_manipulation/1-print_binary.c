0b7ab9421f6838ebd58057e9be7db0d9fcbfdc9b
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-04-08 09:28:05 +0100

    checking for errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,8 +9,16 @@
  */
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-		print_binary(n >> 1);
-
-	_putchar((n & 1) + '0');
+    if (n == 0)
+    {
+        _putchar('0');
+        return;
+    }
+    if (n == 1)
+    {
+        _putchar('1');
+        return;
+    }
+    print_binary(n >> 1);
+    _putchar((n & 1) + '0');
 }
