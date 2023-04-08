09fcee187fa1a936c33df3b6aa8502612c157860
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-04-08 10:20:49 +0100

    fixed betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,14 +2,13 @@
 
 /**
  * print_binary - prints the binary representation of a number.
- *
- * @n - unsigned long int n
+ * @n: unsigned long int n
  *
  * Return: no return
  */
 void print_binary(unsigned long int n)
 {
-   unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
+unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
 	int started = 0;
 
 	if (n == 0)
