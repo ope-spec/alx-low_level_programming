3bf3cf3b305a98b7d5d1802160f5e2e951e0b2a4
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-04-08 12:13:41 +0100

    fixed a bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,45 +1,34 @@
 #include "main.h"
 
-/***
- * create_file - creates a file
+/**
+ * create_file - Creates a new file with the specified content.
+ * @filename: name of the file to create
+ * @text_content: content of the file to create
  *
- * @filename - name of the file to create
- * @text_content - content of the file to create
+ * Return: returns -1 when it fails
  *
- * Return - returns -1 when it fails
- *
- ***/
+ */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, p, len = 0;
+int fd;
+	ssize_t len = 0;
 
-    if (!filename)
-    {
-        return (-1);
-    }
+	if (!filename)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-    }
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    p = write(fd, text_content, len);
+	if (text_content)
+	{
+		while (*(text_content + len))
+			len++;
+		len = write(fd, text_content, len);
+		if (len == -1)
+			return (-1);
+	}
 
-    if (fd == -1)
-    {
-        return (-1);
-    }
-
-    if (text_content)
-        if (p != len)
-        {
-            close(fd);
-            return (-1);
-        }
-
-    close(fd);
-    
-    return (1);
+	close(fd);
+	return (1);
 }
