944cb1e53d9c988737f2bb64c57868b376c32502
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-04-08 09:43:31 +0100

    fixed an error

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,22 +9,27 @@
  */
 void print_binary(unsigned long int n)
 {
-   unsigned long int printbit = 1ul << 63;
-	char c = '0';
+   unsigned long int mask = 1UL << ((sizeof(unsigned long int) * 8) - 1);
+	int started = 0;
 
-	while (!(printbit & n) && printbit != 0)
-		printbit = printbit >> 1;
-
-	if (printbit == 0)
-		write(1, &c, 1);
-
-	while (printbit)
+	if (n == 0)
 	{
-		if (printbit & n)
-			c = '1';
-		else
-			c = '0';
-		write(1, &c, 1);
-		printbit = printbit >> 1;
+		_putchar('0');
+		return;
+	}
+
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
+			_putchar('1');
+			started = 1;
+		}
+		else if (started)
+		{
+			_putchar('0');
+		}
+
+		mask >>= 1;
 	}
 }
