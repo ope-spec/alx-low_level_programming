588142900583c7ae0fc6d72434c378a919aa7ee0
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-04-08 10:38:20 +0100

    fixed an error

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,13 +10,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned long int i = n ^ m;
+unsigned long int diff = n ^ m;
 unsigned int count = 0;
 
-while (i != NULL)
+while (diff)
 {
-count += i & 1;
-i >>= 1;
+count++;
+diff &= (diff - 1);
 }
 
 return (count);
