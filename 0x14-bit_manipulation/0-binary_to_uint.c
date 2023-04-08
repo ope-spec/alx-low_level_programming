d9d98c4f8f1a3881e4d5354fb3ef40b32ce3d075
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-04-08 10:23:47 +0100

    fixed betty style

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,40 +1,40 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int
- * @b: pointer to a string of 0 and 1 chars
- *
- * Return: the converted number, or 0 if there is one or more chars in the
- * string b that is not 0 or 1, or if b is NULL.
- */
+* binary_to_uint - Converts a binary number to an unsigned int
+* @b: pointer to a string of 0 and 1 chars
+*
+* Return: the converted number, or 0 if there is one or more chars in the
+* string b that is not 0 or 1, or if b is NULL.
+*/
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int i;
-    int length, j;
+unsigned int i;
+int length, j;
 
-    if (!b)
-        return (0);
+if (!b)
+return (0);
 
-    i = 0;
+i = 0;
 
-    length = 0;
-    while (b[length] != '\0')
-    {
-        length++;
-    }
+length = 0;
+while (b[length] != '\0')
+{
+length++;
+}
 
-    for (j = 1; length > 0; length--, j *= 2)
-    {
-        if (b[length - 1] != '0' && b[length - 1] != '1')
-        {
-            return (0);
-        }
+for (j = 1; length > 0; length--, j *= 2)
+{
+if (b[length - 1] != '0' && b[length - 1] != '1')
+{
+return (0);
+}
 
-        if (b[length - 1] == '1')
-        {
-            i += j;
-        }
-    }
+if (b[length - 1] == '1')
+{
+i += j;
+}
+}
 
-    return (i);
+return (i);
 }
