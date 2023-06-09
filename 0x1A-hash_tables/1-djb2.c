0aaa86f0fb079f66b8eb372647e67e41e31ccb69
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-06-09 14:10:17 +0100

    djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - Hash function using the djb2 algorithm
- * @str: The string to hash
- * Return: The calculated hash value
- * Description: This function calculates the hash value of a given string
- * using the djb2 algorithm.
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
 
 unsigned long int hash_djb2(const unsigned char *str)
