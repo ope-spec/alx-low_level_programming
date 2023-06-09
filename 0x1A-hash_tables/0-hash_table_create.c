508b61363d95f9db11987a7cea9363a53f4372c8
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-06-09 13:40:40 +0100

    Create Hash Table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: Size of the array
+ *
+ * Return: On success, returns a pointer to the newly created hash table
+ * On failure, returns NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table;
+	unsigned long int index;
+
+	/* Allocate memory for the hash table */
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (new_hash_table == NULL)
+		return (NULL);
+
+	/* Allocate memory for the array */
+	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_hash_table->array == NULL)
+	{
+		free(new_hash_table);
+		return (NULL);
+	}
+
+	new_hash_table->size = size;
+
+	/* Initialize array elements to NULL */
+	for (index = 0; index < size; index++)
+	{
+		new_hash_table->array[index] = NULL;
+	}
+
+	return (new_hash_table);
+}
