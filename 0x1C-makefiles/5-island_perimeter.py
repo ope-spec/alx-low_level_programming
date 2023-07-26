3209406addc64ac7fd37d0a49aec68b24ad74e56
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-07-26 15:32:48 +0100

    Updated code

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""function def island_perimeter(grid): that returns the perimeter of the island described in grid"""
+"""
+function def island_perimeter(grid): that returns 
+the perimeter of the island described in grid
+"""
+
 
 def island_perimeter(grid):
     """
