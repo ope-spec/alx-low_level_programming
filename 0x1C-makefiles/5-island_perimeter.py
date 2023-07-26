49bbaf9554fde3d24de89e5286f93999e9f26e60
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2023-07-26 05:19:01 -0700

    Makefile

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""function def island_perimeter(grid): that returns the perimeter of the island described in grid"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island.
+
+    Args:
+        grid : The grid representing the island.
+
+    Returns:
+        int: perimeter of the island.
+    """
+    if not grid:
+        return 0
+
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
+
+# The code below is for testing the function
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
